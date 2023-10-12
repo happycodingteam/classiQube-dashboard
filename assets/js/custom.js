e6d1e61f2d2e1f47dc762ab0bdce8d458f21ff7d
happycodingteam <happycodding.main@gmail.com>
2023-10-12 14:23:53 +0600

    categori page done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,20 +1,3 @@
-$(document).ready(function(){
-    $("#right-arrow").click(function(){
-
-        $(this).parent(".select-collection-box").addClass("active-s-c-box");
-    });
-
-    $("#down-arrow").click(function(){
-       
-        $(".select-collection-box").removeClass("active-s-c-box");
-    });
-
-
-
-});
-
-
-
 
 $(document).ready(function(){
    
